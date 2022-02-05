b621f993e62fff52c1b0666f3ab256c1f6b33f3b
shreyaSrinivaso <98278627+shreyaSrinivaso@users.noreply.github.com>
2022-02-05 22:39:59 +0530

    Add files via upload

---
+++ stripLstripRstrip.py
@@ -0,0 +1,12 @@
+string=input("Enter a string:")
+
+print(string.strip("*"))
+print(string.rstrip("*"))
+print(string.lstrip("*"))
+
+
+#-------------------------------
+#Enter a string:****python**
+#python
+#****python
+#python**
