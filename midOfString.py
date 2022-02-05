c68170507edbfc9423e2fc2180a4b75b1f55e647
shreyaSrinivaso <98278627+shreyaSrinivaso@users.noreply.github.com>
2022-02-05 22:37:30 +0530

    Add files via upload

---
+++ midOfString.py
@@ -0,0 +1,16 @@
+# Get one string from user
+# identify the middle character of the string
+
+string=input("Enter a string:")
+length=len(string)
+#print(length)
+mid=length//2
+#print(mid)
+print(string[mid])
+
+#--------------------------------------------
+#output:
+#Enter a string:shreya
+#e
+#Enter a string:shrey
+#r
