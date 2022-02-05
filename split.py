3831c6eadb822985d3ee58e0fa051a9972855f53
shreyaSrinivaso <98278627+shreyaSrinivaso@users.noreply.github.com>
2022-02-05 22:43:45 +0530

    Add files via upload

---
+++ split.py
@@ -0,0 +1,12 @@
+a=float(string1.split()[1])
+b=float(string2.split()[1])
+c=float(string3.split()[1])
+
+print(a+b+c)
+
+#-----------------------------  
+#output
+#name<space>float 1:ravi 10.30
+#name<space>float 2:gopi 12.19
+#name<space>float 3:raju 20.20
+#42.69
